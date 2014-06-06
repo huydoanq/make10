c6ac5faa2725da8a90bffec41448f0bbf2af3aa0
Huy Doan <dquochuy@tma.com.vn>
2014-06-06 13:50:20 +0700

    -update

--- app.py
+++ app.py
@@ -8,40 +8,39 @@ import re
 OPERATOR = ["+", "-", "*", "/"]
 
 
-def cal_a_2(array_2):
+def cal_a_2(array):
     """
     Calculate Arrangement list with operators
-    :param array_2: Array with 2 elements
+    :param array: Array with 1 or 2 elements
     :return: String expression list
     """
-    r = list(itertools.permutations(array_2, 2))
-    result = []
-    for op in OPERATOR:
-        for e in r:
-            result.append("(%s %s %s)" % (str(e[0]), op, str(e[1])))
-    return result
+    if len(array) == 1:
+        return array
+    elif len(array) == 2:
+        r = list(itertools.permutations(array, 2))
+        result = []
+        for op in OPERATOR:
+            for e in r:
+                result.append("(%s %s %s)" % (str(e[0]), op, str(e[1])))
+        return result
 
 
 def cal_a_3(first_array, array_3, array_result):
     remain_array = [array_3[i] for i in range(0, len(array_3))
                     if i not in range(0, len(first_array))]
-    if len(first_array) == 2:
-        first_array = cal_a_2(first_array)
-    elif len(first_array) > 2:
+    if len(first_array) > 2:
         first_new = []
         first_array = cal_new(first_new, first_array)
+    else:
+        first_array = cal_a_2(first_array)
     r = []
     result = []
     if len(remain_array) > 2:
         r = cal_new(array_result, remain_array)
-    elif len(remain_array) == 2:
+    else:
         if len(array_result) > 0:
             result = array_result
         r = cal_a_2(remain_array)
-    elif len(remain_array) == 1:
-        if len(array_result) > 0:
-            result = array_result
-        r = remain_array
     for op in OPERATOR:
         for e in r:
             for v in first_array:
@@ -58,10 +57,7 @@ def cal_new(result, array):
     """
     first_array = []
     for i in range(0, len(array) - 1):
-        if i == 0:
-            first_array.append(array[0])
-        else:
-            first_array = [array[j] for j in range(0, i + 1)]
+        first_array = [array[j] for j in range(0, i + 1)]
         result = cal_a_3(first_array, array, result)
 
     return result
@@ -108,6 +104,7 @@ def make_10():
         f.close()
     except Exception as e:
         print(e)
+
 
 if __name__ == "__main__":
     make_10()

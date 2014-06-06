d7348c5265d490446d115dd9f5f5b8b7a31878d1
Huy Doan <quochuy298@gmail.com>
2014-06-06 20:14:45 +0700

    - update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re
 OPERATOR = ["+", "-", "*", "/"]
 
 
-def cal_a_2(array):
+def cal_array_with_operator(array):
     """
     Calculate Arrangement list with operators
     :param array: Array with 1 or 2 elements
@@ -25,22 +25,22 @@ def cal_a_2(array):
         return result
 
 
-def cal_a_3(first_array, array_3, array_result):
+def cal_operator_with_arrangement_arrays(first_array, array_3, array_result):
     remain_array = [array_3[i] for i in range(0, len(array_3))
                     if i not in range(0, len(first_array))]
     if len(first_array) > 2:
         first_new = []
-        first_array = cal_new(first_new, first_array)
+        first_array = get_expression_from_arrangement_array(first_new, first_array)
     else:
-        first_array = cal_a_2(first_array)
+        first_array = cal_array_with_operator(first_array)
     r = []
     result = []
     if len(remain_array) > 2:
-        r = cal_new(array_result, remain_array)
+        r = get_expression_from_arrangement_array(array_result, remain_array)
     else:
         if len(array_result) > 0:
             result = array_result
-        r = cal_a_2(remain_array)
+        r = cal_array_with_operator(remain_array)
     for op in OPERATOR:
         for e in r:
             for v in first_array:
@@ -48,22 +48,21 @@ def cal_a_3(first_array, array_3, array_result):
     return result
 
 
-def cal_new(result, array):
+def get_expression_from_arrangement_array(result, array):
     """
     Star calculation string expression from an arrangement array
     :param result: list of string expression
     :param array: Arrangement array
     :return:
     """
-    first_array = []
     for i in range(0, len(array) - 1):
         first_array = [array[j] for j in range(0, i + 1)]
-        result = cal_a_3(first_array, array, result)
+        result = cal_operator_with_arrangement_arrays(first_array, array, result)
 
     return result
 
 
-def get_expression(result, f):
+def cal_expressions(result, f):
     """
     Calculate the total of string expression
     If total is 10, print it out
@@ -76,7 +75,7 @@ def get_expression(result, f):
             r = eval(t, ns)
             if r == 10:
                 print(t.replace(".0", '') + " = 10")
-                f.write(t + " = 10\n")
+                f.write(t.replace(".0", '') + " = 10\n")
         except Exception as e:
             continue
 
@@ -99,8 +98,8 @@ def make_10():
         f = open("result.txt", "w")
         for i in list_data:
             result = []
-            result = cal_new(result, i)
-            get_expression(result, f)
+            result = get_expression_from_arrangement_array(result, i)
+            cal_expressions(result, f)
         f.close()
     except Exception as e:
         print(e)
